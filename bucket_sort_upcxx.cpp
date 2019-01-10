43a51e61ee1cbe1d05dd7e51409d0ebd76b021af
Bartholomew Tam <b4tam@ucsd.edu>
2019-01-10 15:51:06 -0800

    Changed to vectors

--- bucket_sort_upcxx.cpp
+++ bucket_sort_upcxx.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 const long N = 1000*32;
 #define BLOCK_SIZE (N/upcxx::rank_n())
+const long SORT_MIN = numeric_limits<long>::lowest();
+const long SORT_MAX = numeric_limits<long>::max();
 
 #ifdef DEBUG_
 #define DEBUG_PRINT(x) (cout<<(x)<<endl)
@@ -19,13 +21,13 @@ const long N = 1000*32;
 upcxx::global_ptr<long> section;
 void init_array();
 long find_bucket(long);
-void clean_up(long**);
+void clean_up();
 void allocate_recv_buckets();
 void sort_recv_buckets();
 upcxx::global_ptr<long> get_recv_bucket(long sending_rank, long send_amount);
 
 vector<long> sorted;
-void check_arrays();
+bool check_arrays();
 
 int main(void){
     upcxx::init();
@@ -33,24 +35,14 @@ int main(void){
     
     // Create Buckets
     DEBUG_PRINT("Create Buckets");
-    //vector<long>* buckets = new vector<long>[upcxx::rank_n()];
-    long** buckets = new long*[upcxx::rank_n()];
-    long* bucket_sizes = new long[upcxx::rank_n()];
-    for(long i = 0; i < upcxx::rank_n(); i++){
-        buckets[i] = new long[BLOCK_SIZE];
-        bucket_sizes[i] = 0;
-    }
+    vector<vector<long>> buckets(upcxx::rank_n());
 
     // Sort local buckets
     DEBUG_PRINT("Sort local Buckets");
     for(long i = 0; i < BLOCK_SIZE; i++){
         long correct_bucket = find_bucket(section.local()[i]);
         //cout << i << " " << section.local()[i] << " correct_bucket=" << correct_bucket << endl;
-        if(bucket_sizes[correct_bucket] > BLOCK_SIZE){
-            cout << "Bucket " << correct_bucket << " on rank " << upcxx::rank_me() << " overflowed. " << endl;
-        }
-        buckets[correct_bucket][bucket_sizes[correct_bucket]] = section.local()[i];
-        bucket_sizes[correct_bucket]++;
+        buckets[correct_bucket].push_back(section.local()[i]);
     }
 
     // Allocate receive buckets
@@ -62,8 +54,8 @@ int main(void){
     DEBUG_PRINT("Send Buckets to correct rank");
     upcxx::future<> future = upcxx::make_future();
     for(long i = 0; i < upcxx::rank_n(); i++){
-        upcxx::global_ptr<long> recv_bucket = upcxx::rpc(i, get_recv_bucket, upcxx::rank_me(), bucket_sizes[i]).wait();
-        future = when_all(future, rput(buckets[i], recv_bucket, bucket_sizes[i]));
+        upcxx::global_ptr<long> recv_bucket = upcxx::rpc(i, get_recv_bucket, upcxx::rank_me(), buckets[i].size()).wait();
+        future = when_all(future, rput(&buckets[i][0], recv_bucket, buckets[i].size()));
     }
     future.wait();
     upcxx::barrier();
@@ -75,36 +67,42 @@ int main(void){
 
     // Verify order
     DEBUG_PRINT("Verify Order");
-    check_arrays();
+    bool correct = check_arrays();
+    correct = upcxx::reduce_one(correct, upcxx::op_fast_bit_and, 0).wait();
+    if(upcxx::rank_me() == 0){
+        if(correct){
+            cout << "Sort completed correctly" << endl;
+        } else {
+            cout << "Sort failed" << endl;
+        }
+    }
 
     // Cleanup
     DEBUG_PRINT("Clean Up");
-    clean_up(buckets);
+    clean_up();
     upcxx::finalize();
 }
 
 void init_array(){
     section = upcxx::new_array<long>(BLOCK_SIZE);
     default_random_engine generator;
-    uniform_int_distribution<long> distribution(numeric_limits<long>::lowest(), numeric_limits<long>::max());
+    uniform_int_distribution<long> distribution(SORT_MIN, SORT_MAX);
     for(long i = 0; i < BLOCK_SIZE; i++){
         section.local()[i] = distribution(generator);
     }
 }
 
-upcxx::global_ptr<long>* recv_buckets;
-long* recv_buckets_lengths;
+vector<upcxx::global_ptr<long>> recv_buckets;
+vector<long> recv_buckets_lengths;
 void allocate_recv_buckets(){
-    recv_buckets = new upcxx::global_ptr<long>[upcxx::rank_n()];
-    recv_buckets_lengths = new long[upcxx::rank_n()];
+    recv_buckets.resize(upcxx::rank_n());
+    recv_buckets_lengths.resize(upcxx::rank_n());
 }
 
 void deallocate_recv_buckets(){
-    for(long i = 0; i < upcxx::rank_n(); i++){
-        upcxx::delete_array(recv_buckets[i]);
+    for(auto& bucket : recv_buckets){
+        upcxx::delete_array(bucket);
     }
-    delete[] recv_buckets;
-    delete[] recv_buckets_lengths;
 }
 
 upcxx::global_ptr<long> get_recv_bucket(long sending_rank, long send_amount){
@@ -128,36 +126,32 @@ long get_max_sorted(){
 long get_min_sorted(){
     return sorted.front();
 }
-void check_arrays(){
+bool check_arrays(){
     if(upcxx::rank_me() > 1){
         long prev_max = upcxx::rpc(upcxx::rank_me() - 1, get_max_sorted).wait();
         if(prev_max >= get_min_sorted()){
             cout << RANK_STR << "Sort Failed, " << prev_max << ">=" << get_min_sorted() << endl;
-            return;
+            return false;
         }
     }
     if(upcxx::rank_me() < upcxx::rank_n() - 1){
         long next_min = upcxx::rpc(upcxx::rank_me() + 1, get_min_sorted).wait();
         if(next_min <= get_max_sorted()){
             cout << RANK_STR << "Sort Failed, " << next_min << "<=" << get_max_sorted() << endl;
-            return;
+            return false;
         }
     }
-    cout << RANK_STR << "Sort Completed" << endl;
+    return true;
 }
 
-void clean_up(long** buckets){
-    for(long i = 0; i < upcxx::rank_n(); i++){
-        delete[] buckets[i];
-    }
-    delete[] buckets;
+void clean_up(){
     deallocate_recv_buckets();
 }
 
 long find_bucket(long val){
-    long step = (numeric_limits<long>::max() / upcxx::rank_n() - numeric_limits<long>::lowest() / upcxx::rank_n());
+    long step = (SORT_MAX / upcxx::rank_n() - SORT_MIN / upcxx::rank_n());
     for(long i = 0; i < upcxx::rank_n(); i++){
-        if((val >= numeric_limits<long>::lowest() + (i * step)) && (val < numeric_limits<long>::lowest() + ((i+1) * step))){
+        if((val >= SORT_MIN + (i * step)) && (val < SORT_MAX + ((i+1) * step))){
             return i;
         }
     }
